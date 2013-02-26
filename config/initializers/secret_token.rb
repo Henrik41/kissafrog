db230c19c51443f4d01a3badf464d27ff75abc8d
Henri Knafo <cmoi40@live.ca>
2013-02-25 22:41:59 -0500

    initial project empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kissafrog::Application.config.secret_token = '1d779c4614b9433d4e66d336302791904dd8a767b683e1040e82619adc7d6fac27c3fa7ff1314a7a283eac3c35ccbf272f3b858c6c8ae81ecfe786e4e52f1b5c'
