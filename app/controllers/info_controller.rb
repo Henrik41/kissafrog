4921b8b98d7e134450591124ac210a542dfd7807
Henri Knafo <cmoi40@live.ca>
2013-06-10 21:39:09 -0400

    profile page start

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -4,6 +4,12 @@ class InfoController < ApplicationController
   
   def index
     @user = current_user
+        @albums = @user.albums.all
+        @numpic = (8 - @albums.count)
+        @mainpic = @user.albums.where(:setting => true)
+        
+    @saysomething = @user.saysomething
+        
   end
 
   
